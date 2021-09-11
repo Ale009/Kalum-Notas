e335a72c883da1159e4eca5f3540cd9ce28cc067
“asalazar” <salazarale9@hotmail.com>
2021-09-11 16:57:26 -0600

    modificando carreras técnicas y alumnos controller

--- src/main/java/edu/kalum/notas/core/auth/ResourceServerConfig.java
+++ src/main/java/edu/kalum/notas/core/auth/ResourceServerConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/kalum-notas/v1/carrerasTecnicas").permitAll()
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/kalum-notas/v1/carreras","/kalum-notas/v1/alumnos").permitAll()
                 .anyRequest().authenticated();
     }
 }
